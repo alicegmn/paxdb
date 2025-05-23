1cd8abda0c04516e36bb7907d77908f28e0c7fac
alicegmn <alice.eriksson.561@gmail.com>
2025-05-23 12:04:39 +0300

    blocked adminroutes and requirerole

--- api/routes/adminRoutes.ts
+++ api/routes/adminRoutes.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import authenticateToken, { AuthenticatedRequest } from '../middlewares/authMiddleware.ts';
-import requireRole from '../middlewares/requireRole';
+// import express from 'express';
+// import authenticateToken, { AuthenticatedRequest } from '../middlewares/authMiddleware.ts';
+// import requireRole from '../middlewares/requireRole';
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get('/dashboard', authenticateToken, requireRole('admin'), (req: AuthenticatedRequest, res) => {
-    res.json({ message: 'Welcome to the admin dashboard', user: req.user });
-});
+// router.get('/dashboard', authenticateToken, requireRole('admin'), (req: AuthenticatedRequest, res) => {
+//     res.json({ message: 'Welcome to the admin dashboard', user: req.user });
+// });
 
-export default router;
+// export default router;
